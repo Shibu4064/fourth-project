dbddaee179f217103ee582942c2d90ffe62ed774
Shibu4064 <hrithikmajumdershibu@gmail.com>
2021-11-30 15:59:20 +0600

    updated

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ tabs.forEach(function(tab){
 
 		tab.classList.add("active");
 
-		items.forEach(function(item){
+            items.forEach(function(item){
 			item.style.display = "none";
 		})
 
@@ -30,3 +30,19 @@ tabs.forEach(function(tab){
 
 	})
 })
+
+var acc=document.getElementsByClassName('accordion');
+var i;
+var len=acc.length;
+for(i=0;i<len;i++){
+	acc[i].addEventListener('click',function(){
+		this.classList.toggle('active');
+		var panel=this.nextElementSibling;
+		if(panel.style.maxHeight){
+			panel.style.maxHeight=null;
+		}
+		else{
+			panel.style.maxHeight=panel.scrollHeight+'px'
+		}
+	})
+}
